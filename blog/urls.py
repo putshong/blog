df1128cc09cb3219fd9635d441a58ec1d33fda28
putshong <lsy_9@naver.com>
2018-03-19 21:46:47 +0900

    Add template by block

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
